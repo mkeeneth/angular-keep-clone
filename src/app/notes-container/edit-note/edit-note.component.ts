b5edb0ad28322c4ddf7776fce7e77238095c6a08
Matt Keeneth <mkeeneth@gmail.com>
2019-01-12 22:03:39 -0500

    Add edit note form inputs

--- src/app/notes-container/edit-note/edit-note.component.ts
+++ src/app/notes-container/edit-note/edit-note.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-edit-note',
-  templateUrl: './edit-note.component.html',
-  styleUrls: ['./edit-note.component.css']
+  selector: "app-edit-note",
+  templateUrl: "./edit-note.component.html",
+  styleUrls: ["./edit-note.component.css"]
 })
 export class EditNoteComponent implements OnInit {
+  @Input() id: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  editNote(id: number) {
+    console.log(`Begin edit for note with id ${id}`);
   }
-
 }
